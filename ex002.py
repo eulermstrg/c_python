cbfcb8ed7e704aead273c7ed787cd9e5a417dc98
Euler Magno <eulermagno.mstrg@gmail.com>
2023-05-23 14:41:00 -0300

    Att
    att

---
+++ ex002.py
@@ -0,0 +1,9 @@
+
+nome = input('nome: ')
+print('NOME:', nome)
+ #   print('Sobrenome:', sobrenome)
+  #  print('Idade:', idade)
+   ##print('É maior de idade?', maior_idade)
+    #print('Altura em metros', altura_m)
+
+
